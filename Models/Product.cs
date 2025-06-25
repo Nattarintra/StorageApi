55dcfbd476033bda286139fab1c1e9b1358101b7
Nattaintra <nattarintra.thutsanti@hotmail.com>
2025-06-25 10:51:47 +0200

    Init project

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+       
+        [Required]
+        [StringLength(15)]
+        public string? Name { get; set; }
+
+        [Range(0, 10000)]
+        public int Price { get; set; }
+        public required string Category { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(20)]
+        public string? Shelf { get; set; }
+        public int Count { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string? Description { get; set; }
+    }
+}
